6004deb7bfbbba8b169838a355c29fbab8bd449e
Simon Menke <simon.menke@gmail.com>
2014-06-23 10:32:53 +0200

    Added some documentation for the error types

--- errors.go
+++ errors.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 )
 
+// ErrNotAllOf is returned when a `allOf` keyword failed.
 type ErrNotAllOf struct {
 	Value   interface{}
 	Schemas []*Schema
@@ -33,6 +34,7 @@ func (e *ErrNotAllOf) Error() string {
 	return buf.String()
 }
 
+// ErrNotAnyOf is returned when a `anyOf` keyword failed.
 type ErrNotAnyOf struct {
 	Value   interface{}
 	Schemas []*Schema
@@ -60,6 +62,7 @@ func (e *ErrNotAnyOf) Error() string {
 	return buf.String()
 }
 
+// ErrInvalidDependency is returned when a `dependency` keyword failed.
 type ErrInvalidDependency struct {
 	Property   string
 	Dependency string
@@ -75,6 +78,7 @@ func (e *ErrInvalidDependency) Error() string {
 	}
 }
 
+// ErrInvalidEnum is returned when a `enum` keyword failed.
 type ErrInvalidEnum struct {
 	Expected []interface{}
 	Value    interface{}
@@ -84,6 +88,7 @@ func (e *ErrInvalidEnum) Error() string {
 	return fmt.Sprintf("%v must be in %v", e.Value, e.Expected)
 }
 
+// ErrInvalidFormat is returned when a `format` keyword failed.
 type ErrInvalidFormat struct {
 	Value  interface{}
 	Format string
@@ -93,6 +98,7 @@ func (e *ErrInvalidFormat) Error() string {
 	return fmt.Sprintf("%#v did not match format '%s'", e.Value, e.Format)
 }
 
+// ErrInvalidItem is returned when a `item` keyword failed.
 type ErrInvalidItem struct {
 	Index int
 	Err   error
@@ -102,6 +108,7 @@ func (e *ErrInvalidItem) Error() string {
 	return fmt.Sprintf("Invalid item at %v: %s", e.Index, e.Err)
 }
 
+// ErrTooLarge is returned when a `maximum` keyword failed.
 type ErrTooLarge struct {
 	max       float64
 	exclusive bool
@@ -116,6 +123,7 @@ func (e *ErrTooLarge) Error() string {
 	}
 }
 
+// ErrTooLong is returned when a `maxLength`, `maxItems` or a `maxProperties` keyword failed.
 type ErrTooLong struct {
 	max int
 	was interface{}
@@ -125,6 +133,7 @@ func (e *ErrTooLong) Error() string {
 	return fmt.Sprintf("expected len(%#v) to be smaller than %v", e.was, e.max)
 }
 
+// ErrTooSmall is returned when a `minimum` keyword failed.
 type ErrTooSmall struct {
 	min       float64
 	exclusive bool
@@ -139,6 +148,7 @@ func (e *ErrTooSmall) Error() string {
 	}
 }
 
+// ErrTooShort is returned when a `minLength`, `minItems` or a `minProperties` keyword failed.
 type ErrTooShort struct {
 	min int
 	was interface{}
@@ -148,6 +158,7 @@ func (e *ErrTooShort) Error() string {
 	return fmt.Sprintf("expected len(%#v) to be larger than %v", e.was, e.min)
 }
 
+// ErrNotMultipleOf is returned when a `multipleOf` keyword failed.
 type ErrNotMultipleOf struct {
 	factor float64
 	was    interface{}
@@ -157,6 +168,7 @@ func (e *ErrNotMultipleOf) Error() string {
 	return fmt.Sprintf("expected %#v to be a multiple of %v", e.was, e.factor)
 }
 
+// ErrNotNot is returned when a `not` keyword failed.
 type ErrNotNot struct {
 	Value  interface{}
 	Schema *Schema
@@ -166,6 +178,7 @@ func (e *ErrNotNot) Error() string {
 	return fmt.Sprintf("value must not be valid for: %v", e.Schema)
 }
 
+// ErrNotOneOf is returned when a `oneOf` keyword failed.
 type ErrNotOneOf struct {
 	Value   interface{}
 	Schemas []*Schema
@@ -193,6 +206,7 @@ func (e *ErrNotOneOf) Error() string {
 	return buf.String()
 }
 
+// ErrInvalidPattern is returned when a `pattern` keyword failed.
 type ErrInvalidPattern struct {
 	pattern string
 	was     string
@@ -202,6 +216,7 @@ func (e *ErrInvalidPattern) Error() string {
 	return fmt.Sprintf("expected %#v to be maych %q", e.was, e.pattern)
 }
 
+// ErrInvalidProperty is returned when a `property` keyword failed.
 type ErrInvalidProperty struct {
 	Property string
 	Err      error
@@ -211,6 +226,7 @@ func (e *ErrInvalidProperty) Error() string {
 	return fmt.Sprintf("Invalid property %q: %s", e.Property, e.Err)
 }
 
+// ErrRequiredProperty is returned when a `required` keyword failed.
 type ErrRequiredProperty struct {
 	expected string
 }
@@ -219,6 +235,7 @@ func (e *ErrRequiredProperty) Error() string {
 	return fmt.Sprintf("missing required property: %q", e.expected)
 }
 
+// ErrInvalidType is returned when a `type` keyword failed.
 type ErrInvalidType struct {
 	expected []PrimitiveType
 	was      interface{}
@@ -228,6 +245,7 @@ func (e *ErrInvalidType) Error() string {
 	return fmt.Sprintf("expected type to be in %#v but was %#v", e.expected, e.was)
 }
 
+// ErrNotUnique is returned when a `uniqueItems` keyword failed.
 type ErrNotUnique struct {
 	IndexA int
 	IndexB int
@@ -238,6 +256,7 @@ func (e *ErrNotUnique) Error() string {
 	return fmt.Sprintf("value at %d (%v) is not unique (repeated at %d)", e.IndexA, e.Value, e.IndexB)
 }
 
+// ErrInvalidInstance is returned when the instance is invalid.
 type ErrInvalidInstance struct {
 	Schema *Schema
 	Errors []error
